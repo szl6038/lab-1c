7889f7d03469132eb99e5aee1efc706e0d0deac9
szl6038 <szl6038@psu.edu>
2020-09-02 22:02:41 +0000

    n

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(void) {
   fgets (celsius,256,stdin);
   cel = atof (celsius);
   fah = cel * 9 / 5 + 32;
-  printf ("%f° in Celsius is equivalent to %f° Fahrenheit." , cel, fah);
+  printf ("\n%f° in Celsius is equivalent to %f° Fahrenheit." , cel, fah);
 
   return 0;
 }

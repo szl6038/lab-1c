231e1d1d9d49c0e881e3b71f750430513afcce29
szl6038 <szl6038@psu.edu>
2020-09-03 14:13:58 +0000

    \n

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(void) {
   char* name = readline("Enter temperature in celsius: ");
   cel = atof (name);
   fah = cel * 9 / 5 + 32;
-  printf ("%f° in Celsius is equivalent to %f° Fahrenheit." , cel, fah);
+  printf (\n"%f° in Celsius is equivalent to %f° Fahrenheit." , cel, fah);
 
   return 0;
 }
